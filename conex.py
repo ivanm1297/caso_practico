8f376cee9a9a9090f40f3244c140daacf37ca8c9
ivanm1297 <119536223+ivanm1297@users.noreply.github.com>
2026-06-12 15:29:21 -0600

    Update conex.py

--- conex.py
+++ conex.py
@@ -1,10 +1,12 @@
 import os
 
+DB_NAME = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE") or "railway"
+
 DATABASE_URI = (
     f"mysql+pymysql://"
     f"{os.getenv('MYSQLUSER')}:"
     f"{os.getenv('MYSQLPASSWORD')}@"
     f"{os.getenv('MYSQLHOST')}:"
     f"{os.getenv('MYSQLPORT')}/"
-    f"{os.getenv('MYSQLDATABASE')}"
+    f"{DB_NAME}"
 )
